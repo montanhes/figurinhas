51add8c198846de47eef57fdc4c5e9c86493430e
Ramon Carvalho <ramon@oakz.org>
2017-11-24 17:55:11 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'typeface-roboto'
+import Header from './componentes/header'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
