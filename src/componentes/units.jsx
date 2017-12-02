cde9664a32eff08d82a660f987e01c42369d8193
Sergio Moura <sergio@moura.ca>
2017-12-02 17:53:51 -0500

    Filter and sorting options
    Fixes #2. Fixes #3.

--- src/componentes/units.jsx
+++ src/componentes/units.jsx
@@ -37,14 +37,19 @@ const styles = theme => ({
 
 class Units extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+
         this.state = {
-            unitsListed: Object.values(unitsObj).sort((a, b) => a.name.localeCompare(b.name)),
+            sortField: 'name',
+            filter: '',
             user: null,
             userUnitList: [],
             value: 0,
-        }
-        this.addUnitToMyList = this.addUnitToMyList.bind(this)
+        };
+
+        this.addUnitToMyList = this.addUnitToMyList.bind(this);
+        this.changeFilter = this.changeFilter.bind(this);
+        this.resetFilters = this.resetFilters.bind(this);
     }
 
     handleChange = (event, value) => {
@@ -55,16 +60,35 @@ class Units extends Component {
         this.setState({ value: index });
     };
 
+    updateOwnedList() {
+      if (!this.state.user) return;
+      const refUrl = `ffbexvius/users/${user.uid}/units/`;
+
+      firebase.database()
+        .ref(refUrl)
+        .on('value', snapshot => {
+          console.log(snapshot.val);
+        });
+    }
+
     componentDidMount() {
         auth.onAuthStateChanged((user) => {
+          if (!user) return;
+
+          this.setState(
+            {user},
+            updateOwnedList
+          );
+
+          /*
             if (user) {
-                this.setState({ user })
+                this.setState({ user });
                 firebase.database().ref('ffbexvius/users/'+user.uid+'/units/').on('value', (snapshot)  => {
                     let listUnitsUser = lodash.values(snapshot.val())
                     //this.setState({ userUnitList: listUnitsUser })
                     let tempUserList = listUnitsUser // firebase
                     let tempJsonList = this.state.unitsListed // units.json
-                    /* captura os elementos do firebase em tempUserList compara com o tempJsonList e marca quais devem serem exibidos como ativos ou não usando a classe CSS unit-hide */
+                    // captura os elementos do firebase em tempUserList compara com o tempJsonList e marca quais devem serem exibidos como ativos ou não usando a classe CSS unit-hide
                     for (let j = 0; j < tempUserList.length; j++) {
                         for (let f = 0; f < tempJsonList.length; f++) {
                             if(tempUserList[j].unit_id === tempJsonList[f].unit_id){
@@ -75,7 +99,8 @@ class Units extends Component {
                     this.setState({ unitsListed: tempJsonList})
                 })
             }
-        })
+          */
+        });
     }
 
     addUnitToMyList(e, unit_id) {
@@ -88,15 +113,16 @@ class Units extends Component {
         console.log(unit_id)
         let clickedUnit = document.getElementById(unit_id)
         console.log(clickedUnit.dataset.own)
+        const refUrl = `ffbexvius/users/${this.state.user.uid}/units/${unit_id}`;
         if (clickedUnit.dataset.own === 'false') {
             console.log('entrei aqui')
-            firebase.database().ref('ffbexvius/users/'+this.state.user.uid+'/units/'+unit_id).set({
+            firebase.database().ref(refUrl).set({
                 own: true,
                 unit_id: unit_id
             })
             document.getElementById(unit_id).classList.remove("unit-hide")
         } else {
-            firebase.database().ref('ffbexvius/users/'+this.state.user.uid+'/units/'+unit_id).set({
+            firebase.database().ref(refUrl).set({
                 own: false,
                 unit_id: unit_id
             })
@@ -104,19 +130,70 @@ class Units extends Component {
         }
     }
 
-    renderUnitsList(unitsListed, star) {
+    renderUnitsList(star) {
+        const unitsListed = Object.keys(unitsObj).map(key => unitsObj[key]);
         const filteredUnits = star > 0
             ? unitsListed.filter(unit => unit.rarity_min === star)
-            : unitsListed.filter(unit => unit.own === true);
+            : unitsListed;
 
-        return filteredUnits.map(unit => (
-            <div key={unit.unit_id} className='unit'>
-                <a href='' onClick={(evt) => this.addUnitToMyList(evt, unit.unit_id)}>
-                    <img id={unit.unit_id} src={unit.img} alt={unit.name} className={unit.own ? 'unit-img' : 'unit-img unit-hide'} data-own={unit.own === true ? true : false} />
-                </a>
-                <div><a href={'https://exvius.gamepedia.com/'+unit.name.replace(/\s/g, "_")}>{unit.name}</a></div>
+        const remainingUnits = filteredUnits
+            .filter(unit => {
+                if (!this.state.filter || this.state.filter === '') return true;
+
+                return unit.name.toLowerCase().indexOf(this.state.filter.toLowerCase()) >= 0;
+            })
+            .sort((a, b) => {
+                const fieldName = this.state.sortField;
+                return a[fieldName].localeCompare(b[fieldName]);
+            })
+            .map(unit => (
+                <div key={unit.unit_id} className='unit'>
+                    <a href='' onClick={(evt) => this.addUnitToMyList(evt, unit.unit_id)}>
+                        <img id={unit.unit_id} src={unit.img} alt={unit.name} className={unit.own ? 'unit-img' : 'unit-img unit-hide'} data-own={unit.own === true ? true : false} />
+                    </a>
+                    <div><a href={'https://exvius.gamepedia.com/'+unit.name.replace(/\s/g, "_")}>{unit.name}</a></div>
+                </div>
+            ));
+
+        if (remainingUnits.length > 0) return remainingUnits;
+
+        return (
+            <div>
+                No units to display
             </div>
-        )   )
+        );
+    }
+
+    resetFilters() {
+      this.setState({
+        filter: '',
+        sortField: 'name',
+      });
+    }
+
+    changeFilter(event) {
+      this.setState({
+        [event.target.name]: event.target.value,
+      });
+    }
+
+    renderFilter() {
+      return (
+        <div className="filter-bar">
+          <span>
+            <label>Filter</label>
+            <input type="text" name="filter" onChange={this.changeFilter} value={this.state.filter} />
+          </span>
+          <span><select name="sortField" value={this.state.sortField} onChange={this.changeFilter}>
+            <option value="name">Name</option>
+            <option value="unit_id">ID</option>
+            <option value="job">Job</option>
+          </select></span>
+          <span>
+            <button onClick={this.resetFilters}>Clear Filters</button>
+          </span>
+        </div>
+      )
     }
 
     render() {
@@ -141,33 +218,39 @@ class Units extends Component {
                     </AppBar>
                     <SwipeableViews axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'} index={this.state.value} onChangeIndex={this.handleChangeIndex}>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 5)}
+                                {this.renderUnitsList(5)}
                             </div>
                         </TabContainer>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 4)}
+                                {this.renderUnitsList(4)}
                             </div>
                         </TabContainer>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 3)}
+                                {this.renderUnitsList(3)}
                             </div>
                         </TabContainer>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 2)}
+                                {this.renderUnitsList(2)}
                             </div>
                         </TabContainer>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 1)}
+                                {this.renderUnitsList(1)}
                             </div>
                         </TabContainer>
                         <TabContainer dir={theme.direction}>
+                            {this.renderFilter()}
                             <div className='unitList'>
-                                {this.renderUnitsList(this.state.unitsListed, 0)}
+                                {this.renderUnitsList(0)}
                             </div>
                         </TabContainer>
                     </SwipeableViews>
